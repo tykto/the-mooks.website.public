596ec399f0fc147b93c5312f09a72de2cb37228a
Andrew Robertson <andrewcrobertson@gmail.com>
2020-12-04 10:55:03 +1100

    chore: add error/layout

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+  <meta name="author" content="The Mooks" />
+  <meta name="description" content="Comedy troupe" />
+  <meta name="keywords" content="Comedy, Music, Sketch" />
+  <meta name="robots" content="index, follow" />
+  <title>The Mooks</title>
+</svelte:head>
+
+<slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,12 +2,4 @@
   import HomePage from '@app/pages/Home';
 </script>
 
-<svelte:head>
-  <meta name="author" content="The Mooks" />
-  <meta name="description" content="Comedy troupe" />
-  <meta name="keywords" content="Comedy, Music, Sketch" />
-  <meta name="robots" content="index, follow" />
-  <title>The Mooks</title>
-</svelte:head>
-
 <HomePage />
